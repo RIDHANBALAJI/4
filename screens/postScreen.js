32daee9238f3cac8e62657f9881e19a8d66a1f68
RIDHANBALAJI <117922290+RIDHANBALAJI@users.noreply.github.com>
2023-09-08 21:11:55 +0530

    Add files via upload

---
+++ screens/postScreen.js
@@ -0,0 +1,23 @@
+import react from "react";
+import { Text,View,TouchableOpacity,StyleSheet } from "react-native";
+
+export default class postScreen extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        
+            flex: 1,
+            backgroundColor:"black"
+    },
+        
+    
+})
